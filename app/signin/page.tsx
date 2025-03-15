52d48f31104c340b0528eafd6a507d93c3729dc2
Jaesung Nam <overpowerman@gmail.com>
2025-03-15 14:05:56 +0900

    ewe

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,16 +2,30 @@
 
 import {useEffect} from "react";
 import {signIn} from "next-auth/react";
+import {useSearchParams} from "next/navigation";
 
 const Page = () => {
+  const searchParams = useSearchParams();
   useEffect(() => {
     (async () => {
+      const callbackUrl = searchParams.get('callback-url')
+      if(callbackUrl) {
+        window.location.href = callbackUrl;
+        return;
+      }
       const resp = await signIn('keycloak')
       if(resp?.ok) {
         console.log(resp)
       }
     })();
   }, [])
+
+  return <div>
+    <button onClick={() => {
+
+    }}>로그인</button>
+
+  </div>
 }
 
 export default Page
