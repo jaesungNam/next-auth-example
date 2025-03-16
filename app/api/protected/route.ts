a263d59e52934c48e4983f9dd9f841cdf2f5d1a4
Jaesung Nam <overpowerman@gmail.com>
2025-03-16 17:29:05 +0900

    rest

--- app/api/protected/route.ts
+++ app/api/protected/route.ts
@@ -1,8 +1,21 @@
 import { auth } from "auth"
 
-export const GET = auth((req) => {
+const API_SERVER = 'http://localhost:9090'
+
+export const GET = auth(async (req) => {
   if (req.auth) {
-    return Response.json({ data: "Protected data" })
+    try {
+      const resp = await fetch(API_SERVER, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${req.auth.accessToken}`
+        }
+      })
+      const json = await resp.text();
+      return Response.json(json)
+    } catch (e) {
+      return Response.json({message: 'error!!'}, {status: 500})
+    }
   }
 
   return Response.json({ message: "Not authenticated" }, { status: 401 })
